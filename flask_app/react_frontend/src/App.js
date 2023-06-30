cffb948504360c3b94f7c9896fdf1220b216f5c5
EthanWakeford <ethanwakeford@gmail.com>
2023-06-30 16:06:44 -0500

    added logging

--- flask_app/react_frontend/src/App.js
+++ flask_app/react_frontend/src/App.js
@@ -6,13 +6,14 @@ import useToken from './usetoken';
 export default function App() {
   const [userData, setUserData] = useState();
   const [token, setToken] = useState(getToken());
-  const authCode = getAuthCode();
+  const [authCode, setAuthCode] = useState(getAuthCode());
 
   function getMe(token, authCode) {
+    console.log('fetching');
     return fetch(
       '/api/me?' +
         new URLSearchParams({
-          authCode: (token ? '' : authCode),
+          authCode: token ? '' : authCode,
           refreshToken: token,
         })
     );
@@ -20,32 +21,39 @@ export default function App() {
 
   useEffect(
     function () {
-      if (!token && !authCode) {
+      console.log('effecting', authCode);
+      if (userData || (!token && !authCode)) {
+        console.log('no api call');
         return;
       }
-      console.log('effecting');
-        getMe(token, authCode)
-          .then((res) => res.json())
-          // .then(() => {
-          //   setUserData({ display_name: 'Ethan' });
-          // });
-          .then((data) => {
-            console.log(data);
-            if (!token || token === 'null') {
-              /* if refresh token does NOT exist already in local storage, add
+      const code = authCode;
+      setAuthCode('');
+      console.log('getting me');
+      getMe(token, authCode)
+        .then((res) => {
+          console.log(res.status);
+          return res.json();
+        })
+        // .then(() => {
+        //   setUserData({ display_name: 'Ethan' });
+        // });
+        .then((data) => {
+          console.log(data);
+          if (!token || token === 'null') {
+            /* if refresh token does NOT exist already in local storage, add
           returned refresh token to local storage */
-              const refreshToken = data.refresh_token;
-              localStorage.setItem('refreshToken', refreshToken);
-              setToken(refreshToken);
-            }
-            setUserData(JSON.parse(data.response));
-          })
-          .catch((error) => {
-            alert('login to spotify has failed');
-            console.error('login error: ', error)
-          })
+            const refreshToken = data.refresh_token;
+            localStorage.setItem('refreshToken', refreshToken);
+            setToken(refreshToken);
+          }
+          setUserData(JSON.parse(data.response));
+        })
+        .catch((error) => {
+          alert('login to spotify has failed');
+          console.error('login error: ', error);
+        });
     },
-    [authCode, token]
+    [authCode, token, userData]
   );
 
   if (userData) {
@@ -56,6 +64,7 @@ export default function App() {
 }
 
 function getAuthCode() {
+  // gets authcode from url, returns empty string if not found
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const authCode = urlParams.get('code');
@@ -63,6 +72,7 @@ function getAuthCode() {
 }
 
 function getToken() {
+  // gets refresh token from local storage, returns empty string if not found
   const refreshToken = localStorage.getItem('refreshToken');
   if (refreshToken && refreshToken !== 'null') {
     return refreshToken;
