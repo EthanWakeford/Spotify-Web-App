6866ae2271d55d957e95108c7998f942b15b1044
EthanWakeford <ethanwakeford@gmail.com>
2023-06-29 23:06:35 -0500

    only slightly broken currently

--- flask_app/react_frontend/src/App.js
+++ flask_app/react_frontend/src/App.js
@@ -1,7 +1,7 @@
-import "./App.css";
-import { useEffect, useState } from "react";
-import { LoggedIn, LoggedOut } from "./components/loggedIn";
-import useToken from "./usetoken";
+import './App.css';
+import { useEffect, useState } from 'react';
+import { LoggedIn, LoggedOut } from './components/loggedIn';
+import useToken from './usetoken';
 
 export default function App() {
   const [userData, setUserData] = useState();
@@ -10,7 +10,7 @@ export default function App() {
 
   function getMe(token, authCode) {
     return fetch(
-      "/api/me?" +
+      '/api/me?' +
         new URLSearchParams({
           authCode: authCode,
           refreshToken: token,
@@ -23,23 +23,27 @@ export default function App() {
       if (!token && !authCode) {
         return;
       }
-      console.log("effecting");
+      console.log('effecting');
       try {
         getMe(token, authCode)
           .then((res) => res.json())
+          // .then(() => {
+          //   setUserData({ display_name: 'Ethan' });
+          // });
           .then((data) => {
-            if (!token || token === "null") {
+            console.log(data);
+            if (!token || token === 'null') {
               /* if refresh token does NOT exist already in local storage, add
-            returned refresh token to local storage */
+          returned refresh token to local storage */
               const refreshToken = data.refresh_token;
-              localStorage.setItem("refreshToken", refreshToken);
+              localStorage.setItem('refreshToken', refreshToken);
               setToken(refreshToken);
             }
             setUserData(JSON.parse(data.response));
           });
       } catch (error) {
-        alert("login to spotify has failed");
-        console.error("login error: ", error);
+        alert('login to spotify has failed');
+        console.error('login error: ', error);
       }
     },
     [authCode, token]
@@ -55,14 +59,14 @@ export default function App() {
 function getAuthCode() {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const authCode = urlParams.get("code");
+  const authCode = urlParams.get('code');
   return authCode;
 }
 
 function getToken() {
-  const refreshToken = localStorage.getItem("refreshToken");
-  if (refreshToken && refreshToken !== "null") {
+  const refreshToken = localStorage.getItem('refreshToken');
+  if (refreshToken && refreshToken !== 'null') {
     return refreshToken;
   }
-  return "";
+  return '';
 }
