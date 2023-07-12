3559181433d773da307960a726aa99f41deec925
EthanWakeford <ethanwakeford@gmail.com>
2023-07-11 23:52:29 -0500

    calls to backend now working, everything running smoothly

--- flask_app/react_frontend/src/App.js
+++ flask_app/react_frontend/src/App.js
@@ -8,30 +8,22 @@ export default function App() {
 
   useEffect(
     function () {
-      if (userData) {
-        console.log('no api call');
-        return;
-      }
       apiHandler
         .getMe()
+        // .then((res) => {
+        //   // console.log(res)
+        //   return res
+        // })
         .then((res) => res.json())
         .then((data) => {
           setUserData(JSON.parse(data.response));
+          apiHandler.saveRefreshToken(data.refresh_token);
         })
         .catch((err) => {
           console.log(err);
         });
-      // .then((data) => {
-      //   console.log(data);
-      //   if (!token || token === 'null') {
-      //     /* if refresh token does NOT exist already in local storage, add
-      //   returned refresh token to local storage */
-      //
-      //   }
-      //   setUserData(JSON.parse(data.response));
-      // })
     },
-    [userData]
+    []
   );
 
   if (userData) {
@@ -39,29 +31,4 @@ export default function App() {
   } else {
     return <LoggedOut />;
   }
-}
-
-function getAuthCode() {
-  // gets authcode from url, returns empty string if not found
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const authCode = urlParams.get('code');
-  return authCode;
-}
-
-function getToken() {
-  // gets refresh token from local storage, returns empty string if not found
-  const refreshToken = localStorage.getItem('refreshToken');
-  if (refreshToken && refreshToken !== 'null') {
-    return refreshToken;
-  }
-  return '';
-}
-
-function refreshTokenSetter(refreshToken) {
-  // sets the refresh token into local storage if not already present
-  // (or new one exists)
-
-  localStorage.setItem('refreshToken', refreshToken);
-  // setToken(refreshToken);
 }
--- flask_app/react_frontend/src/services/myService.js
+++ flask_app/react_frontend/src/services/myService.js
@@ -3,22 +3,26 @@
 
 class MyService {
   #AuthCodeUseCounter = 0;
-  #AuthCode = getAuthCode();
-  #RefreshToken = getRefreshToken();
+  #AuthCode = this.getAuthCode();
+  #RefreshToken = this.getRefreshToken();
 
   getMe() {
     // queries the serverside api, prevents the authcode from being used
     // more than once
 
+    if (!this.#AuthCode && ! this.#RefreshToken) {
+      return Promise.reject('no login creds')
+    }
+
     if (this.#AuthCode && !this.#RefreshToken) {
       if (this.#AuthCodeUseCounter > 0) {
+        this.#AuthCode = '';
         return Promise.reject('authcode can only be used once');
       }
 
       this.#AuthCodeUseCounter += 1;
     }
 
-    console.log(this.#AuthCodeUseCounter);
     return fetch(
       '/api/me?' +
         new URLSearchParams({
@@ -27,23 +31,32 @@ class MyService {
         })
     );
   }
-}
 
-function getAuthCode() {
-  // gets authcode from url, returns empty string if not found
-  const queryString = window.location.search;
-  const urlParams = new URLSearchParams(queryString);
-  const authCode = urlParams.get('code');
-  return authCode;
-}
+  saveRefreshToken(refreshToken) {
+    const oldToken = this.getRefreshToken();
 
-function getRefreshToken() {
-  // gets refresh token from local storage, returns empty string if not found
-  const refreshToken = localStorage.getItem('refreshToken');
-  if (refreshToken && refreshToken !== 'null') {
-    return refreshToken;
+    if (!oldToken || oldToken !== refreshToken) {
+      localStorage.setItem('refreshToken', refreshToken);
+      this.#RefreshToken = refreshToken
+    }
   }
-  return '';
+
+  getAuthCode() {
+    // gets authcode from url, returns empty string if not found
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const authCode = urlParams.get('code');
+    return authCode;
+  }
+
+  getRefreshToken() {
+    // gets refresh token from local storage, returns empty string if not found
+    const refreshToken = localStorage.getItem('refreshToken');
+    if (refreshToken && refreshToken !== 'null') {
+      return refreshToken;
+    }
+    return '';
+  }
 }
 
 const apiHandler = new MyService();
