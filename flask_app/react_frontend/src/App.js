81d41c2396b78e6a238916b0cde750c9b6e70c84
EthanWakeford <ethanwakeford@gmail.com>
2023-07-12 00:34:37 -0500

    small code cleanup

--- flask_app/react_frontend/src/App.js
+++ flask_app/react_frontend/src/App.js
@@ -6,25 +6,18 @@ import apiHandler from './services/myService';
 export default function App() {
   const [userData, setUserData] = useState();
 
-  useEffect(
-    function () {
-      apiHandler
-        .getMe()
-        // .then((res) => {
-        //   // console.log(res)
-        //   return res
-        // })
-        .then((res) => res.json())
-        .then((data) => {
-          setUserData(JSON.parse(data.response));
-          apiHandler.saveRefreshToken(data.refresh_token);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
-    []
-  );
+  useEffect(function () {
+    apiHandler
+      .getMe()
+      .then((res) => res.json())
+      .then((data) => {
+        setUserData(JSON.parse(data.response));
+        apiHandler.saveRefreshToken(data.refresh_token);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   if (userData) {
     return <LoggedIn userData={userData} />;
