5984eb04850263b72dc8713c034adc69952b32de
EthanWakeford <ethanwakeford@gmail.com>
2023-08-14 12:21:03 -0500

    redirection now functioning properly

--- react_app/src/components/App.jsx
+++ react_app/src/components/App.jsx
@@ -1,32 +1,33 @@
 import '../App.css';
 import { useEffect, useState } from 'react';
 import { LoggedIn, LoggedOut } from './loggedIn';
-import apiHandler from '../services/myService';
+// import apiHandler from '../services/myService';
 import { isEmpty } from 'lodash';
 
 export default function App() {
-  const [userData, setUserData] = useState({});
+  // const [userData, setUserData] = useState({});
 
-  useEffect(function () {
-    apiHandler
-      .getMe()
-      .then((res) => {
-        console.log(res);
-        return res.json();
-      })
-      .then((data) => {
-        apiHandler.setUserId(JSON.parse(data.response).id);
-        setUserData(JSON.parse(data.response));
-        apiHandler.saveRefreshToken(data.refresh_token);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+  // useEffect(function () {
+  //   apiHandler
+  //     .getMe()
+  //     .then((res) => {
+  //       console.log(res);
+  //       return res.json();
+  //     })
+  //     .then((data) => {
+  //       apiHandler.setUserId(JSON.parse(data.response).id);
+  //       setUserData(JSON.parse(data.response));
+  //       apiHandler.saveRefreshToken(data.refresh_token);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
 
-  if (!isEmpty(userData)) {
-    return <LoggedIn userData={userData} />;
-  } else {
-    return <LoggedOut />;
-  }
+  // if (!isEmpty(userData)) {
+  //   return <LoggedIn userData={userData} />;
+  // } else {
+  //   return <LoggedOut />;
+  // }
+  return <LoggedOut></LoggedOut>
 }
